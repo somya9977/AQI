d0c732b95d64b3e95849aa97fdf93ce4a18bbcf4
somya9977 <somyathakur040403@gmail.com>
2025-05-31 16:05:17 +0530

    Configure Vite for GitHub Pages

--- FrontEnd-production/vite.config.js
+++ FrontEnd-production/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Replace '/AQI/' with your repo name
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/https://somya9977.github.io/AQI/',
+});
